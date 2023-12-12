9ce9a1f9a5375891503a2453e4b47e2043bdd212
N1ewb <nathaniellucero20@gmail.com>
2023-12-12 23:14:31 +0800

    Mbile UPdate smol patch 1.2b

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom'
-import { AuthProvider, useAuth } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
 import './Header.css'
+
 import NeatlistLogo from '../images/neatlist-transparent.png'
+import MenuIcon from '../images/icons8-menu-white-50.png'
+
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
-  const auth = useAuth()
-  const navigate = useNavigate() 
 
   const handleClick = (e, sectionId) => {
     if (location.pathname !== '/') {
@@ -41,11 +41,6 @@ const Header = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  
-  const handleSignout = async () => {
-    await auth.SignOut()
-    navigate('/')
-  }
 
 
   const toggleDropdown = () => {
@@ -64,31 +59,49 @@ const Header = () => {
                 <div className="spacer"></div>
                 <div className='nav-links'>
                   <div className='unauth-header-container' style={{ backgroundColor: scrolled ? '#23252b' : 'transparent', height: '70px', width: '100%', position: 'fixed', top: 0, left: 0 }}>
-                    <div className='unauth-nav-bar'>
-                        <Link to="/" style={{textDecoration: 'none'}}>
-                            <div className='logo-wrapper'> 
-                                <img src={NeatlistLogo} alt='neatlist' width='150px' />
-                            </div>
-                        </Link>
-                        <div className='auth-nav-links'>
-                        
-                            <a href="#third-section" onClick={(e) => handleClick(e, 'third-section')}>Features</a>
-                            <a href="#fourth-section" onClick={(e) => handleClick(e, 'fourth-section')}>About</a>
-                            <Link to='/Dashboard'>Dashboard</Link>
-                            <div className="dropdown">
-                              <button onClick={toggleDropdown} className="dropbtn">
-                                Profile
-                              </button>
-                              {isOpen && (
-                                <div className="dropdown-content">
-                                  <p>{auth.currentUser.email}</p>
-                                  <button onClick={()=>handleSignout()}>Logout</button>
-                                </div>
-                              )}
-                            </div>
-                            
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                        <div className="logo-wrapper-header">
+                          <img src={NeatlistLogo} alt="neatlist" width="150px" />
                         </div>
+                      </Link>
+                  <div className="unauth-nav-bar">
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                      <div className="logo-wrapper">
+                        <img src={NeatlistLogo} alt="neatlist" width="150px" />
+                      </div>
+                    </Link>
+                    <div className="auth-nav-links-always-open">
+                      
+                         <Link to="/Dashboard" style={{borderBottom:'1px solid white'}}>Dashboard</Link>
+                        <a  href="#third-section" onClick={(e) => handleClick(e, "third-section")}>
+                          Features
+                        </a>
+                        <a href="#fourth-section" onClick={(e) => handleClick(e, "fourth-section")}>
+                          About
+                        </a>
+                       
+                      </div>
+                    <div
+                      className="header-dropdown-menu"
+                      onClick={toggleDropdown}
+                    >
+                      <img src={MenuIcon} alt='menu' height='30px' />
                     </div>
+
+                    {isOpen && ( // Display links only when menu is open
+                      <div className="auth-nav-links">
+                         <Link to="/Dashboard" style={{borderBottom:'1px solid white'}}>Dashboard</Link>
+                        <a  href="#third-section" onClick={(e) => handleClick(e, "third-section")}>
+                          Features
+                        </a>
+                        <a href="#fourth-section" onClick={(e) => handleClick(e, "fourth-section")}>
+                          About
+                        </a>
+                       
+                      </div>
+                    )}
+                  </div>
+
                   </div>
                 </div>
             </div>
--- src/components/UnauthHeader.jsx
+++ src/components/UnauthHeader.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
+
+import NeatlistLogo from '../images/neatlist-transparent.png'
+import MenuIcon from '../images/icons8-menu-white-50.png'
+
 import './UnauthHeader.css'
 const UnauthHeader = () => {
     const [scrolled, setScrolled] = useState(false);
     const location = useLocation();
     const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = (e, sectionId) => {
     if (location.pathname !== `/`) {
@@ -19,6 +24,10 @@ const UnauthHeader = () => {
         section.scrollIntoView({ behavior: 'smooth' });
       }
     }
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
   };
 
 
@@ -41,17 +50,50 @@ const UnauthHeader = () => {
   return (
     <>
         <div className='unauth-header-container' style={{ backgroundColor: scrolled ? '#23252b' : 'transparent', height: '70px', width: '100%', position: 'fixed', top: 0, left: 0 }}>
-            <div className='unauth-nav-bar'>
-                <Link to="/" style={{textDecoration: 'none'}}>
-                    <div className='logo-wrapper'> 
-                        <p>Neat <span>List</span></p>
+        <Link to="/" style={{ textDecoration: "none" }}>
+                        <div className="logo-wrapper-header">
+                          <img src={NeatlistLogo} alt="neatlist" width="150px" />
+                        </div>
+                      </Link>
+            <div className="unauth-nav-bar">
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                      <div className="logo-wrapper">
+                        <img src={NeatlistLogo} alt="neatlist" width="150px" />
+                      </div>
+                    </Link>
+                    <div className="auth-nav-links-always-open">
+                      
+                         <Link to="/Dashboard" style={{borderBottom:'1px solid white'}}>Dashboard</Link>
+                        <a  href="#third-section" onClick={(e) => handleClick(e, "third-section")}>
+                          Features
+                        </a>
+                        <a href="#fourth-section" onClick={(e) => handleClick(e, "fourth-section")}>
+                          About
+                        </a>
+                       
+                      </div>
+                    <div
+                      className="header-dropdown-menu"
+                      onClick={toggleDropdown}
+                    >
+                      <img src={MenuIcon} alt='menu' height='30px' />
                     </div>
-                </Link>
-                <div className='nav-links'>
-                    <a href="#third-section" onClick={(e) => handleClick(e, 'third-section')}>Features</a>
-                    <a href="#fourth-section" onClick={(e) => handleClick(e, 'fourth-section')}>About</a>
-                </div>
-            </div>
+
+                    {isOpen && ( 
+                      <div className="auth-nav-links">
+                        <Link to='LoginPage'>Login</Link>
+                        <Link to='SignupPage'>Sign Up</Link>
+                        <a  href="#third-section" onClick={(e) => handleClick(e, "third-section")}>
+                          Features
+                        </a>
+                        <a href="#fourth-section" onClick={(e) => handleClick(e, "fourth-section")}>
+                          About
+                        </a>
+                       
+                      </div>
+                    )}
+                  </div>
+
         </div>
     </>
   )
